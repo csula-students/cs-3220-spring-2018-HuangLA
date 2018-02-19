2c470d001ab8284ee530f51a4de96c57c9e75a7a
Huang <xuhuang@gmail.com>
2018-02-18 23:51:54 -0800

    finishedlab2

--- lab2/app.js
+++ lab2/app.js
@@ -1,6 +1,28 @@
+window.incrementalGame = {
+	state: {
+		counter: 0
+		
+	}
+};
 
 function increaseNum() {
-    var ori_num = document.getElementById("dis-num").value;
-    ori_num++;
-    document.getElementById("dis-num").value = ori_num;
+    var counter = document.getElementById("dis-num").value;
+    counter++;
+    document.getElementById("dis-num").value = counter;
+}
+
+
+class PubSub{
+	constructor() {
+		this.subscribers = [];
+	}
+
+	subscribe(fn){
+		this.subscribers.push(fn);
+	}
+	publish (data){
+		this.subscribers.forEach(subscriber => {
+			subscriber(data);
+		});
+	}
 }
