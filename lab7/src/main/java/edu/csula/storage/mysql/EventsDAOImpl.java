ba9f3d8bb8af27015b366bc9e85bc51d2e9e4040
Huang <xuhuang@gmail.com>
2018-05-06 23:38:57 -0700

    eventsDAOImpl

--- lab7/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
+++ lab7/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
@@ -14,10 +14,10 @@ public class EventsDAOImpl implements EventsDAO {
 	// TODO: fill the Strings with the SQL queries as "prepated statements" and
 	//       use these queries variable accordingly in the method below
 	protected static final String getAllQuery = "SELECT * FROM EVENTS";
-	protected static final String getByIdQuery = "SELECT * FROM EVENTS" + "WHERE ID = ?";
-	protected static final String setQuery = "UPDATE EVENTS SET NAME = ?, description =?, trigger_at = ?" + "WHERE ID = ?;";
-	protected static final String addQuery = "INSERT INTO EVENTS" + "VALUES (?,?,?,?,?);";
-	protected static final String removeQuery = "";
+	protected static final String getByIdQuery = "SELECT * FROM EVENTS WHERE ID = ?";
+	protected static final String setQuery = "UPDATE EVENTS SET NAME = ?, description =?, trigger_at = ? + WHERE ID = ?";
+	protected static final String addQuery = "INSERT INTO EVENTS VALUES (?,?,?,?,?)";
+	protected static final String removeQuery = "DELECT FROM EVENTS WHERE ID= ?";
 
 	public EventsDAOImpl(Database context) {
 		this.context = context;
@@ -26,27 +26,77 @@ public class EventsDAOImpl implements EventsDAO {
 	@Override
 	public List<Event> getAll() {
 		// TODO: get all events from jdbc
-		return new ArrayList<>();
+		List<Event> result = new ArrayList<>();
+		try (Connection c = context.getConnection(); Statement stmt = c.createStatement()) {
+			ResultSet rs = stmt.executeQuery(getAllQuery);
+			while (rs.next()) {
+				Event entry = new Event(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4));
+				result.add(entry);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+		return result;
 	}
 
 	@Override
 	public Optional<Event> getById(int id) {
 		// TODO: get specific event by id
-		return Optional.empty();
+		try (Connection c = context.getConnection(); Statement stmt = c.createStatement()) {
+			Optional<Event> actualEvent = Optional.empty();
+			PreparedStatement ps = c.prepareStatement(getByIdQuery);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			Event entry = new Event(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4));
+			actualEvent = Optional.of(entry);
+			return actualEvent;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Optional.empty();
+		}
 	}
 
 	@Override
 	public void set(int id, Event event) {
-		// TODO: update specific event by id
+		try (Connection c = context.getConnection(); Statement stmt = c.createStatement()) {
+			PreparedStatement ps = c.prepareStatement(setQuery);
+			ps.setString(1, event.getName());
+			ps.setString(2, event.getDescription());
+			ps.setInt(3, event.getTriggerAt());
+			ps.setInt(4, id);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void add(Event event) {
 		// TODO: implement jdbc logic to add a new event
+		try (Connection c = context.getConnection(); Statement stmt = c.createStatement()) {
+			PreparedStatement ps = c.prepareStatement(addQuery);
+			ps.setInt(1, event.getId());
+			ps.setString(2, event.getName());
+			ps.setString(3, event.getDescription());
+			ps.setInt(4, event.getTriggerAt());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void remove(int id) {
 		// TODO: implement jdbc logic to remove event by id
+		try (Connection c = context.getConnection(); PreparedStatement ps = c.prepareStatement(removeQuery)) {
+			ps.setInt(1, id);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+
+		}
+	}
 	}
 }
